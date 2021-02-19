7afaf1df29a2fe4532f39bae75a9c2e2892ede0e
wonjinYi <studioplug17@gmail.com>
2021-02-20 01:42:07 +0900

    #1.3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import "./styles.css";
 import React, { useState } from "react";
 import { useInput } from "./hooks/useInput";
+import { useTabs } from "./hooks/useTabs";
+
+const content = [
+  {
+    tab: "section 1",
+    content: "content 1"
+  },
+  {
+    tab: "section 2",
+    content: "content 2"
+  }
+];
 
 export default function App() {
-  const maxLen = (value) => !value.includes("@");
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
+
   return (
     <div className="App">
-      <h1>hellow</h1>
-      <input placeholder="Name" {...name} />
+      {content.map((section, index) => (
+        <button
+          onClick={() => {
+            changeItem(index);
+          }}
+        >
+          {section.tab}
+        </button>
+      ))}
+      <div> {currentItem.content} </div>
     </div>
   );
 }
