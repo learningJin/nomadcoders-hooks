72ea2273b4e206e52cb5faabd39b411eb30e3823
wonjinYi <studioplug17@gmail.com>
2021-02-19 23:33:37 +0900

    #1.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./styles.css";
+import React, { useState } from "react";
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+
+  return { value, onChange };
+};
 export default function App() {
+  const name = useInput("Mr.");
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>hellow</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
